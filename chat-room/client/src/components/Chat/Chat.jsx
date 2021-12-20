e9efac31a335691e15f754862eacc970c123f3aa
ronco1812 <89574940+ronco1812@users.noreply.github.com>
2021-12-20 13:08:29 +0200

    init

--- chat-room/client/src/components/Chat/Chat.jsx
+++ chat-room/client/src/components/Chat/Chat.jsx
@@ -1,7 +1,19 @@
-import MsgArea from "./massage/MsgArea";
+import MsgArea from "./message/MsgArea";
 import OnlineList from "./onlineUsers/OnlineList";
 import TextArea from "./TextArea/TextArea";
+import axios from "axios";
+import { useState } from "react";
+
 export default function Chat(){
+    const[messages, setMessages] = useState([])
+    const getMessages = async() =>{
+        try {
+            const {data} = axios.get("http://localhost:8080/send/message")
+            setMessages(data);
+        } catch (error) {
+             setMessages([]);
+        }
+    }
     return(
         <div>
             <span>
