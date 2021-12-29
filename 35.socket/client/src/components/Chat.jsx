d5fca60b0a833d27a4f8577f5f0feb7e346f00ac
ronco1812 <89574940+ronco1812@users.noreply.github.com>
2021-12-29 17:00:55 +0200

    added logged list

--- 35.socket/client/src/components/Chat.jsx
+++ 35.socket/client/src/components/Chat.jsx
@@ -1,38 +1,55 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useRef, useState, useContext } from "react";
 import TextField from "@material-ui/core/TextField";
-import React, {useContext, useEffect, useRef, useState } from "react";
 import io from "socket.io-client";
 import { nanoid } from "nanoid";
+import axios from "axios";
 import notyf from "../notyf";
 import nameContext from "../contexts/name/context";
 
+const server = "http://localhost:4000";
 
-function Chat() {
-  const {name} = useContext(nameContext)
+export default function Chat() {
+  const { name } = useContext(nameContext);
+
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([]);
+  const [list, setList] = useState([]);
 
   const socketRef = useRef();
 
   useEffect(() => {
-    socketRef.current = io.connect("http://localhost:4000");
+    axios.post(`${server}?name=${name}`);
 
-    socketRef.current.on("messageBack", ({ name, message }) => {
-      setChat((prevState) => {
-        return [...prevState, { name, message }];
-      });
+    socketRef.current = io.connect(server);
+
+    socketRef.current.on("loggedList", (loggedList) => {
+      setList(loggedList);
     });
 
+    socketRef.current.on("joined", (person) => {
+      notyf.success(`${person} has just joined the chat`);
+    });
+
+    socketRef.current.on("disconnected", (person) => {
+      notyf.success(`${person} has just left the chat`);
+    });
+
+    socketRef.current.on("messageBack", (messageObj) => {
+      setChat((prevState) => [...prevState, messageObj]);
+    });
     // return () => socketRef.current.disconnect();
   }, []);
 
-  const onTextChange = ({target: {value}}) => {
+  const onTextChange = ({ target: { value } }) => {
     setMessage(value);
   };
 
   const onMessageSubmit = (e) => {
     e.preventDefault();
-    if(!message){
-      notyf.error("please make sure you enter content")
+    if (!message) {
+      notyf.error("Please make sure you enter content");
+      return;
     }
     socketRef.current.emit("message", { name, message });
     setMessage("");
@@ -68,8 +85,11 @@ function Chat() {
         <h1>Chat Log</h1>
         {renderChat()}
       </div>
+      <ul style={{ border: "5px solid red", display: "inline-grid" }}>
+        {list.map((person, i) => (
+          <li key={i}>{person}</li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default Chat;
--- 35.socket/server/index.js
+++ 35.socket/server/index.js
@@ -1,19 +1,40 @@
+//#region DB
+const logged = [];
+const connections = [];
+//#endregion
 const app = require("express")();
+app.post("/", (req, res) => {
+  const { name } = req.query;
+  logged.push(name);
+  res.sendStatus(201);
+});
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-
 io.on("connection", (socket) => {
+  const index = connections.push(socket.id) - 1;
+
+  connections.forEach((id) => {
+    if (id !== socket.id) {
+      io.to(id).emit("joined", logged[index]);
+    }
+  });
+
+  io.emit("loggedList", logged);
+
   socket.on("message", ({ name, message }) => {
-    if(!message || !name) return
+    if (!message || !name) return;
     io.emit("messageBack", { name, message });
   });
 
   socket.on("disconnect", () => {
-    io.emit("messageBack", { name: "wow", message: "render" });
+    connections.splice(index, 1);
+    const leaving = logged.splice(index, 1);
+    io.emit("loggedList", logged);
+    io.emit("disconnected", leaving[0]);
   });
 });
 
-http.listen(4000, function () {
+http.listen(4000, () => {
   console.log("listening on port 4000");
 });
